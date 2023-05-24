057935aaaadd0e4a0e21e26804934dd6f43e7a01
jinxOAO <819166336@qq.com>
2023-05-24 18:41:23 +0800

    version2.2.7

--- src/Configs.cs
+++ src/Configs.cs
@@ -9,7 +9,7 @@ namespace DSP_Battle
 {
     public class Configs
     {
-        public static string versionString = "2.2.6";
+        public static string versionString = "2.2.7";
         public static string qq = "694213906";
         public static bool developerMode = false; //发布前务必修改！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
         
